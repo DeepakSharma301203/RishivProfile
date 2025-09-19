25688635714fa3696a8269b5dd33a075ee3a9916
DeepakSharma301203 <147157298+DeepakSharma301203@users.noreply.github.com>
2025-09-19 23:09:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import PublicLayout from './Layout/PublicLayout';
 import HomePage from './Page/HomePage/page';
 import ContactPage from './Page/ContactPage/page';
 import FormPage from './Page/FormPage/page'
-import AuthPage from './Page/AuthPage/page';
 
 function App() {
   return (
@@ -15,10 +14,9 @@ function App() {
         {/* This is the parent route that renders your Layout */}
         <Route path="/" element={<PublicLayout />}>
           <Route path='/' element={<HomePage />} />
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/contact' element={<ContactPage />} />
-          <Route path='/form' element={<FormPage />} />
-          <Route path='/login' element={<AuthPage />} />
+          <Route path='/Home' element={<HomePage />} />
+          <Route path='/Contact' element={<ContactPage />} />
+          <Route path='/Form' element={<FormPage />} />
         </Route>
       </Routes>
     </Router>
